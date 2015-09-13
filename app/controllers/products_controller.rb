20160191019f6442b2e93fa63b7bbe4bb332bed8
German Escobar <german.escobarc@gmail.com>
2015-09-13 16:35:36 -0500

    Implement list and create products

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,2 +1,18 @@
 class ProductsController < ApplicationController
+  def index
+    @products = Product.all
+  end
+
+  def new
+    @product = Product.new
+  end
+
+  def create
+    @product = Product.create(product_params)
+  end
+
+  private
+    def product_params
+      params.require(:product).permit(:name, :price)
+    end
 end
